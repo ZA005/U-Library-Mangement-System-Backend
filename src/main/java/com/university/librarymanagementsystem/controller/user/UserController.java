8adfe6b2cfb1c0e744344e7a3486746708320a96
gd-salle <gerardojr.salle@gmail.com>
2025-02-25 16:44:50 +0800

    refactored: updated sending otp

--- src/main/java/com/university/librarymanagementsystem/controller/user/UserController.java
+++ src/main/java/com/university/librarymanagementsystem/controller/user/UserController.java
@@ -1,5 +1,6 @@
 package com.university.librarymanagementsystem.controller.user;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.university.librarymanagementsystem.dto.user.UserDTO;
+import com.university.librarymanagementsystem.service.user.OTPService;
 import com.university.librarymanagementsystem.service.user.UserService;
 
 import lombok.AllArgsConstructor;
@@ -18,8 +20,26 @@ public class UserController {
 
     private UserService service;
 
-    @GetMapping("{id}")
+    private OTPService otp;
+
+    @GetMapping("/verify/{id}")
     public ResponseEntity<UserDTO> fetchUserById(@PathVariable("id") String user_id) {
-        return ResponseEntity.ok(service.fetchUserById(user_id));
+        try {
+            UserDTO user = service.fetchUserById(user_id);
+            if (user == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND); // 404 if user not found
+            }
+
+            String otpCode = otp.generateOTP();
+            otp.storeOTP(user.getEmailAdd(), otpCode);
+            otp.sendOTPEmail(user.getEmailAdd(), otpCode);
+
+            // Return the user with a 200 OK status
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (Exception e) {
+            // Return an error response if something goes wrong
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
 }
--- src/main/java/com/university/librarymanagementsystem/service/user/UserService.java
+++ src/main/java/com/university/librarymanagementsystem/service/user/UserService.java
@@ -4,4 +4,5 @@ import com.university.librarymanagementsystem.dto.user.UserDTO;
 
 public interface UserService {
     UserDTO fetchUserById(String user_id);
+
 }
