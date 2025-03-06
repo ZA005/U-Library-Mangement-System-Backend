3917cbdb4f15db57ab15d8479f68e16a7e9db85a
gd-salle <gerardojr.salle@gmail.com>
2025-03-06 14:31:08 +0800

    wip: book acquisition

--- src/main/java/com/university/librarymanagementsystem/entity/catalog/Acquisition.java
+++ src/main/java/com/university/librarymanagementsystem/entity/catalog/Acquisition.java
@@ -1,5 +1,6 @@
 package com.university.librarymanagementsystem.entity.catalog;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,25 +25,36 @@ public class Acquisition {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(name = "book_title", nullable = false)
     private String book_title;
 
+    @Column(name = "isbn", nullable = false)
     private String ISBN;
 
+    @Column(name = "publisher", nullable = false)
     private String publisher;
 
+    @Column(name = "edition", nullable = false)
     private String edition;
 
+    @Column(name = "published_date", nullable = false)
     private String published_date;
 
+    @Column(name = "purchase_date", nullable = false)
     private String purchase_date;
 
+    @Column(name = "acquired_date", nullable = false)
     private String acquired_date;
 
+    @Column(name = "vendor", nullable = false)
     private String vendor;
 
+    @Column(name = "vendor_location", nullable = false)
     private String vendor_location;
 
+    @Column(name = "funding_source", nullable = false)
     private String funding_source;
 
+    @Column(name = "status", nullable = false)
     private int status;
 }
