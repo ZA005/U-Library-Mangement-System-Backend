288217e7862b47715d8126380c937266dba0a6a6
gd-salle <gerardojr.salle@gmail.com>
2025-03-06 18:10:34 +0800

    feat: added all functionality of acquisition

--- src/main/java/com/university/librarymanagementsystem/entity/catalog/Acquisition.java
+++ src/main/java/com/university/librarymanagementsystem/entity/catalog/Acquisition.java
@@ -2,8 +2,6 @@ package com.university.librarymanagementsystem.entity.catalog;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -22,23 +20,25 @@ import lombok.Setter;
 public class Acquisition {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "book_title", nullable = false)
     private String book_title;
 
-    @Column(name = "isbn", nullable = false)
-    private String ISBN;
+    @Column(name = "isbn")
+    private String isbn;
 
-    @Column(name = "publisher", nullable = false)
+    @Column(name = "publisher")
     private String publisher;
 
-    @Column(name = "edition", nullable = false)
+    @Column(name = "edition")
     private String edition;
 
-    @Column(name = "published_date", nullable = false)
+    @Column(name = "published_date")
     private String published_date;
+
+    @Column(name = "purchase_price", nullable = false)
+    private float purchase_price;
 
     @Column(name = "purchase_date", nullable = false)
     private String purchase_date;
@@ -46,15 +46,15 @@ public class Acquisition {
     @Column(name = "acquired_date", nullable = false)
     private String acquired_date;
 
-    @Column(name = "vendor", nullable = false)
+    @Column(name = "vendor")
     private String vendor;
 
-    @Column(name = "vendor_location", nullable = false)
+    @Column(name = "vendor_location")
     private String vendor_location;
 
-    @Column(name = "funding_source", nullable = false)
+    @Column(name = "funding_source")
     private String funding_source;
 
-    @Column(name = "status", nullable = false)
+    @Column(name = "status")
     private int status;
 }
---
+++ src/main/java/com/university/librarymanagementsystem/repository/catalog/AcquisitionRepository.java
@@ -0,0 +1,27 @@
+package com.university.librarymanagementsystem.repository.catalog;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.university.librarymanagementsystem.entity.catalog.Acquisition;
+
+import jakarta.transaction.Transactional;
+
+@Repository
+public interface AcquisitionRepository extends JpaRepository<Acquisition, Integer> {
+
+    // this query is for fetching all records that status 0 or havent caloged yet
+    @Query(value = "SELECT * FROM acquisition WHERE status = 0", nativeQuery = true)
+    List<Acquisition> getPendingRecords();
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE acquisition SET status = 1 WHERE id = :id", nativeQuery = true)
+    int updatePendingStatus(@Param("id") Integer id);
+
+}
