2f25aa46e652a60acabda5743889419053670b11
gd-salle <gerardojr.salle@gmail.com>
2025-03-21 20:31:46 +0800

    MINOR CHANGES

---
+++ src/main/java/com/university/librarymanagementsystem/entity/circulation/TransactionHistory.java
@@ -0,0 +1,52 @@
+package com.university.librarymanagementsystem.entity.circulation;
+
+import java.time.LocalDateTime;
+
+import com.university.librarymanagementsystem.enums.TransactionType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "transaction_history")
+public class TransactionHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "transaction_type", nullable = false)
+    private TransactionType transactionType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "loan_id")
+    private Loan loan;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reservation_id")
+    private Reservation reservation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fine_id")
+    private Fine fine;
+
+    @Column(name = "transaction_date", nullable = false)
+    private LocalDateTime transactionDate;
+}
