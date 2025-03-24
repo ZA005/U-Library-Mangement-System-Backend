20329c7731939c85cb173aa6ded5d66ce18c7540
gd-salle <gerardojr.salle@gmail.com>
2025-03-24 19:43:48 +0800

    update: adjusted saving of transaciton, saving new loan and repo for fetching unique record

--- src/main/java/com/university/librarymanagementsystem/repository/circulation/TransactionRepository.java
+++ src/main/java/com/university/librarymanagementsystem/repository/circulation/TransactionRepository.java
@@ -1,9 +1,17 @@
 package com.university.librarymanagementsystem.repository.circulation;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.university.librarymanagementsystem.entity.circulation.TransactionHistory;
+import com.university.librarymanagementsystem.enums.TransactionType;
 
 public interface TransactionRepository extends JpaRepository<TransactionHistory, Integer> {
+    @Query("SELECT t FROM TransactionHistory t WHERE t.loan.id = :id AND t.transactionType = :transactionType")
+    Optional<TransactionHistory> findByLoanAndTransactionType(@Param("id") Integer id,
+            @Param("transactionType") TransactionType transactionType);
 
 }
--- src/main/java/com/university/librarymanagementsystem/repository/user/UserRepository.java
+++ src/main/java/com/university/librarymanagementsystem/repository/user/UserRepository.java
@@ -1,11 +1,14 @@
 package com.university.librarymanagementsystem.repository.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.university.librarymanagementsystem.entity.user.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-
+    @Query(value = "SELECT email_address FROM users WHERE id = :id", nativeQuery = true)
+    String fetchEmailById(@Param("id") String id);
 }
